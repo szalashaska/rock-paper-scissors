1757cd8ace01543dc13723ce0c372f4a75fbfc84
szalashaska <kamilpetryniak@gmail.com>
2022-12-11 22:08:41 +0100

    Added result

--- src/components/Contest.tsx
+++ src/components/Contest.tsx
@@ -1,16 +1,19 @@
 import Button from "./Button";
 import "./Contest.scss";
+import Result from "./Result";
 
 function Contest() {
   return (
     <div className="contest-container">
       <div className="contest__choice player-choice">
-        <p className="contest-text">YOU PICKED</p>
+        <h2 className="contest-text">YOU PICKED</h2>
         <Button avatar="scissors" picked />
       </div>
-      <div className="result" />
+      <div className="result-container">
+        <Result />
+      </div>
       <div className="contest__choice ai-choice">
-        <p className="contest-text">THE HOUSE PICKED</p>
+        <h2 className="contest-text">THE HOUSE PICKED</h2>
         <Button avatar="scissors" picked />
       </div>
     </div>
---
+++ src/components/Result.tsx
@@ -0,0 +1,14 @@
+import "./Result.scss";
+
+function Result() {
+  return (
+    <>
+      <h2 className="result-text">YOU LOSE</h2>
+      <button className="result-button" type="button">
+        PLAY AGAIN
+      </button>
+    </>
+  );
+}
+
+export default Result;
